b2cb79a24ec52df5a457374a9028a9dae7076af4
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-09-23 12:18:39 +0700

    update

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ server.set('port', process.env.PORT || 3000);
 /**
  * register site collections
  */
-server.use('/transport', express.static(path.join(__dirname, 'transport'), { index: 'default.html' }));
-const webpath = path.join(__dirname, 'transport'); 
+//server.use('/transport', express.static(path.join(__dirname, 'transport'), { index: 'default.html' }));
+const webpath = path.join(__dirname, 'public'); 
 server.use('/', express.static(webpath, { index: 'default.html' }));
 server.use('/about-us', express.static(webpath , { index: 'about-us.html' }));
 server.use('/contact-us', express.static(webpath, { index: 'contact-us.html' }));
