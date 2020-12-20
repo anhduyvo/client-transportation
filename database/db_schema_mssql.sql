f9fa58ac4c4fc6efb843d3beebea5235e69abc55
devinsg <duyanh2005@gmail.com>
2020-12-20 15:49:18 +0700

    - make upload images

--- database/db_schema_mssql.sql
+++ database/db_schema_mssql.sql
@@ -6,13 +6,3 @@ GO
 
 SET QUOTED_IDENTIFIER ON
 GO
-
---
--- Alter table [dbo].[Product]
---
-ALTER TABLE [dbo].[Product] ADD COLUMN Title AS [nvarchar](50) DEFAULT NULL
-
---
--- Alter table [dbo].[User]
---
-ALTER TABLE [dbo].[User] ADD COLUMN Title AS [nvarchar](50) DEFAULT NULL
