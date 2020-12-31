fc8c2d6ccd2e124ccd61987747ddb462cac86c80
Anh <voduyanh1984@gmail.com>
2020-12-31 16:31:54 +0700

    - tweak priority

--- modules/products/server/routes/products.server.routes.js
+++ modules/products/server/routes/products.server.routes.js
@@ -12,14 +12,14 @@ module.exports = function (app) {
     .get(products.list)
     .post(products.create);
 
+  app.route('/api/products/image').all(productsPolicy.isAllowed)
+    .post(products.changeProductImage);
+
   // Single product routes
   app.route('/api/products/:productId').all(productsPolicy.isAllowed)
     .get(products.read)
     .put(products.update)
     .delete(products.delete);
-
-  app.route('/api/products/image').all(productsPolicy.isAllowed)
-    .post(products.changeProductImage);
 
   // Finish by binding the product middleware
   app.param('productId', products.productByID);
